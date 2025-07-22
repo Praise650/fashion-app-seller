f18a7da00673f20b4f4f7f90e338e3121895fa9c
Praise650 <ayokanmi1275@gmail.com>
2025-07-22 21:19:12 +0100

    added images

---
+++ lib/app/app_setup.locator.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedLocatorGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs, implementation_imports, depend_on_referenced_packages
+
+import 'package:get_it/get_it.dart';
+
+import '../core/services/notification_service.dart';
+
+final locator = GetIt.instance;
+
+Future<void> setupLocator({String? environment}) async {
+// Register dependencies
+//   locator.registerLazySingleton(() => LocationService());
+//   locator.registerLazySingleton(() => GeoFenceService());
+//   locator.registerLazySingleton(() => NotificationService());
+}
---
+++ lib/app/res/images.dart
@@ -0,0 +1,12 @@
+class Images {
+  static const String _imagePath = "assets/images";
+  static const String _logoPath = "assets/logos";
+
+  static const normal = "$_imagePath/normal.jpg";
+  static const terrain = "$_imagePath/terrain.jpg";
+  static const satellite = "$_imagePath/satellite.jpg";
+  static const hybrid = "$_imagePath/hybrid.jpg";
+
+  /// logos
+  static const appLogo = "$_logoPath/app-logo.jpg";
+}
